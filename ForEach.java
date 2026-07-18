e519e6af47da4deae0294ed74af86ee6452864a1
sagn-ng <sangnn2510@gmail.com>
2026-07-18 13:36:06 +0700

    just learned a few examples of the "for each" loop

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach {
+    public static void main(String[] args) {
+        String[] cars = {"BMW", "Lamborghini", "MacLaren"};
+        for (String car : cars){
+            System.out.println(car);
+        }
+        System.out.print("\n");
+        int[] arr={10, 20, 30, 40, 50};
+        for (int num : arr){
+            System.out.println(num);
+        }
+    }
+}
