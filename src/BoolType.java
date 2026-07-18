a79532b03f876beab2b6b3890349b8bb14219c81
sagn-ng <sangnn2510@gmail.com>
2026-07-18 10:53:59 +0700

    first commit

---
+++ src/BoolType.java
@@ -0,0 +1,8 @@
+public class BoolType {
+    public static void main(String[] args) {
+        boolean x=true;
+        System.out.println(x);
+        int a=3, b=6; boolean c=a>b;
+        System.out.println("The value of a>b is: " + c);
+    }
+}
---
+++ src/Print.java
@@ -0,0 +1,12 @@
+public class Print {
+    // public class' name should be uppercase (Naming Conventions)
+	public static void main(String[]args) {
+	    System.out.println("My name is A");//normal println()
+        System.out.println("2 * 5 = " + (2*5));
+        System.out.println("25 / 10 = " + (25/10)); //"integer type"
+        System.out.println("25 / 10 = " + (25/10.0));
+        System.out.println("25 / 10 = " + (25.0/10));
+        System.out.println("25 / 10 = " + (25.0/10.0) + "\n");
+        //the bottom 3s are "float type"
+	}
+}
