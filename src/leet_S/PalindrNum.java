54ac6b368fd6a27967d95e700dd46d1c909f77f4
sagn-ng <sangnn2510@gmail.com>
2026-07-19 21:01:59 +0700

    leetcode 9 (palindrome number)

---
+++ src/leet_S/PalindrNum.java
@@ -0,0 +1,19 @@
+package leet_S;
+import java.util.Scanner;
+public class PalindrNum {
+    public static boolean isPalindrome(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int a=0;
+        while (x>a){
+            a=10*a+(x%10);
+            x/=10;
+        }
+        return (x==a || x==a/10);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.close();
+        System.out.println(isPalindrome(n));
+    }
+}
