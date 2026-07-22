6cc16539e17142a0205c83a4203249d231dbc63e
sagn-ng <sangnn2510@gmail.com>
2026-07-22 16:37:48 +0700

    leetcode 205 (isomorphic strings)

---
+++ leet_S/IsomorphicStr.java
@@ -0,0 +1,30 @@
+package leet_S;
+import java.util.Scanner;
+public class IsomorphicStr {
+    public static boolean isIsomorphic(String s, String t){
+        int[] visited_s=new int[256];
+        int[] visited_t=new int[256];
+        char[] func=new char[256];
+        if (s.length()!=t.length()) return false;
+        for (int i=0; i<s.length(); i++){
+            if (visited_s[s.charAt(i)]==0){
+                if (visited_t[t.charAt(i)]==1) return false;
+                else{
+                    visited_s[s.charAt(i)]=1;
+                    visited_t[t.charAt(i)]=1;
+                    func[s.charAt(i)]=t.charAt(i);
+                }
+            }
+            else if (func[s.charAt(i)]!=t.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine(), t=sc.nextLine();
+        sc.close();
+
+        System.out.println(isIsomorphic(s, t));
+    }
+}
